559a517cc5b4fdbda99e0ff48fda823006b1c4ca
AC <ragnarokdoom@gmail.com>
2017-11-14 10:42:52 +0800

    mobs info V2

--- app/src/main/java/com/example/alucardc/idlegame/MainActivity.java
+++ app/src/main/java/com/example/alucardc/idlegame/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.alucardc.idlegame;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.app.ProgressDialog;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar PB;
+    int i =5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PB = (ProgressBar)findViewById(R.id.pbtest);
+
+        ObjectAnimator animation = ObjectAnimator.ofInt(PB, "progress", 0, 100);
+        animation.setDuration(5000);
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) { }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                //do something when the countdown is complete
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) { }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) { }
+        });
+        animation.start();
     }
 }
--- app/src/main/java/com/example/alucardc/idlegame/mobs.java
+++ app/src/main/java/com/example/alucardc/idlegame/mobs.java
@@ -6,14 +6,45 @@ package com.example.alucardc.idlegame;
 
 public class mobs {
     class mobs001{
-        String name = "Poring";
-        int healthPoint = 10;       //需要點擊次數
-        int actionBarTime = 5000;   //動作條時間,毫秒
-        int guardPoint = 1;         //破防需要次數
-        int elementTypes = 2;       //題目屬性種類
-        int elementQuestionRange = 3;   //題目難度,屬性球數量3-6
-        int[] weakPoint = {1,3};        //弱點屬性(火1,水2,木3,光4,暗5,心6)
-        int stunDuration = 5000;    //暈眩時間,毫秒
+        String[] name = {"Poring","波利"};
+        int healthPoint = 10;               //需要點擊次數
+        int actionBarTime = 5000;           //動作條時間,毫秒
+        int guardPoint = 1;                 //破防需要次數
+        int elementTypes = 2;               //題目屬性種類
+        int elementQuestionRange = 3;       //題目難度,屬性球數量3-6
+        int[] weakPoint = {1,3};            //弱點屬性(火1,水2,木3,光4,暗5,心6)
+        int stunDuration = 5000;            //暈眩時間,毫秒
+        double differenceRange = 0.1;       //個體差異程度,正負 min(基礎值*(1-差異度)) max(基礎*(1+差異度))
+        int weakPointEffect = 2000;         //弱點屬性對於暈眩時間增加幅度,毫秒
+        double damage = 1;                  //對玩家傷害
+    }
+
+    class mobs002{
+        String[] name = {"Orc","獸人"};
+        int healthPoint = 20;               //需要點擊次數
+        int actionBarTime = 5000;           //動作條時間,毫秒
+        int guardPoint = 1;                 //破防需要次數
+        int elementTypes = 2;               //題目屬性種類
+        int elementQuestionRange = 3;       //題目難度,屬性球數量3-6
+        int[] weakPoint = {1,2};            //弱點屬性(火1,水2,木3,光4,暗5,心6)
+        int stunDuration = 3000;            //暈眩時間,毫秒
+        double differenceRange = 0.2;       //個體差異程度,正負 min(基礎值*(1-差異度)) max(基礎*(1+差異度))
+        int weakPointEffect = 1000;         //弱點屬性對於暈眩時間增加幅度,毫秒
+        double damage = 3;                  //對玩家傷害
+    }
+
+    class mobs003{
+        String[] name = {"Peco","大嘴鳥"};
+        int healthPoint = 10;               //需要點擊次數
+        int actionBarTime = 5000;           //動作條時間,毫秒
+        int guardPoint = 1;                 //破防需要次數
+        int elementTypes = 2;               //題目屬性種類
+        int elementQuestionRange = 3;       //題目難度,屬性球數量3-6
+        int[] weakPoint = {2,5};            //弱點屬性(火1,水2,木3,光4,暗5,心6)
+        int stunDuration = 5000;            //暈眩時間,毫秒
+        double differenceRange = 0.1;       //個體差異程度,正負 min(基礎值*(1-差異度)) max(基礎*(1+差異度))
+        int weakPointEffect = 1500;         //弱點屬性對於暈眩時間增加幅度,毫秒
+        double damage = 2;                  //對玩家傷害
     }
 
 }
