663f4275d823dc62be71824630093dc03be9fd3f
Archmono <ragnarokdoom@gmail.com>
2017-12-11 00:26:52 +0800

    move Main data to Loading

--- app/src/main/java/com/example/alucardc/idlegame/Loading.java
+++ app/src/main/java/com/example/alucardc/idlegame/Loading.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -25,6 +27,10 @@ import java.util.Calendar;
  */
 
 public class Loading extends Activity {
+
+    String TAG = "MainDataTest";
+    String TAG2 = "MainPlayerTest";
+
     /*↓SharedPreferences裡儲存的資料名稱↓*/
     public static final String DATE_PREF = "DATE_PREF";
     public static final String PREF_OLD_TIME = "DATE_OldTime";
@@ -34,7 +40,18 @@ public class Loading extends Activity {
 
     /*↓SQLite取出的資料↓*/
     public static ArrayList idList = new ArrayList();
+    public static ArrayList nameList = new ArrayList();
+    public static ArrayList healthPointList = new ArrayList();
     public static ArrayList rarityList = new ArrayList();
+    public static ArrayList speedList = new ArrayList();
+    public static ArrayList qCountsList = new ArrayList();
+    public static ArrayList qTypesList = new ArrayList();
+    public static ArrayList qRangeList = new ArrayList();
+    public static ArrayList stunTimeList = new ArrayList();
+    public static ArrayList atkList = new ArrayList();
+    public static ArrayList imageList = new ArrayList();
+    public static ArrayList lootsList = new ArrayList();
+    public static ArrayList lootsDropRateList = new ArrayList();
     /*↑SQLite取出的資料↑*/
 
     int timeGap,nextTime; //運算用變數
@@ -42,6 +59,45 @@ public class Loading extends Activity {
     String tempOldTime,tempNextTime="0"; //暫存用變數
     String[] tempHasMod = new String[6]; //暫存用變數
     Calendar rightNow = Calendar.getInstance(); //提取時間用的方法
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  //隱藏狀態列
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        DBInfo.DB_FILE = getDatabasePath("idlegame.db")+"";    //database的絕對路徑
+        copyDBFile();
+        newTime = rightNow.getTimeInMillis();
+        restorePrefs();
+        DateTest();
+        Log.d("LOGDATE_Mobs", tempHasMod[0] +" "+ tempHasMod[1] +" "+ tempHasMod[2] +" "+ tempHasMod[3] +" "+ tempHasMod[4] +" "+ tempHasMod[5]+" ");
+        onSave();
+
+        getPlayerStatus();
+
+//        Log.d(TAG, idList.toString());
+//        Log.d(TAG, nameList.toString());
+//        Log.d(TAG, healthPointList.toString());
+//        Log.d(TAG, rarityList.toString());
+//        Log.d(TAG, speedList.toString());
+//        Log.d(TAG, qCountsList.toString());
+//        Log.d(TAG, qTypesList.toString());
+//        Log.d(TAG, qRangeList.toString());
+//        Log.d(TAG, stunTimeList.toString());
+//        Log.d(TAG, atkList.toString());
+//        Log.d(TAG, imageList.toString());
+//        int[] loots = (int[]) lootsList.get(0);
+//        Log.d(TAG, loots[0]+", "+loots[1]+", "+loots[2]);
+//        int[] lootsDP = (int[]) lootsDropRateList.get(0);
+//        Log.d(TAG, lootsDP[0]+", "+lootsDP[1]+", "+lootsDP[2]);
+    }
+
+    @Override
+    protected void onDestroy() {
+        onSave();
+        super.onDestroy();
+    }
 
     private void restorePrefs() { //讀取的位置
         tempHasMod = new String[]{"0", "0", "0", "0", "0", "0"}; //暫存重置不知為何new在class外部會有問題所以先寫在這裡
@@ -71,27 +127,6 @@ public class Loading extends Activity {
         settings.edit().putString(DATE_NEXT_DATE, String.valueOf(nextTime)).commit();
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  //隱藏狀態列
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loading);
-        DBInfo.DB_FILE = getDatabasePath("idlegame.db")+"";    //database的絕對路徑
-        copyDBFile();
-        newTime = rightNow.getTimeInMillis();
-        restorePrefs();
-        DateTest();
-        Log.d("LOGDATE_Mobs", tempHasMod[0] +" "+ tempHasMod[1] +" "+ tempHasMod[2] +" "+ tempHasMod[3] +" "+ tempHasMod[4] +" "+ tempHasMod[5]+" ");
-        onSave();
-    }
-
-    @Override
-    protected void onDestroy() {
-        onSave();
-        super.onDestroy();
-    }
-
     int random;
     void DateTest () {
         timeGap = (int)(newTime-oldTime)/1000; //計算上次開啟遊戲與本次開啟遊戲時間差
@@ -99,7 +134,7 @@ public class Loading extends Activity {
         Log.d("LOGDATE_NextTime", nextTime+"");
         for(int i=0;i<6;i++) { //迴圈檢查，如怪物陣列滿了將不會進入
             if (tempHasMod[i].equals("0")) { //如果怪物陣列裡的ID為0
-                getRarityData("1"); //取得場景1一隨機怪物ID (變數存在RandomTest.cId)
+                getData("1"); //取得場景1一隨機怪物ID (變數存在RandomTest.cId)
                 random = (int) (Math.random() * 25) + 5; //隨機 5-30秒
                 if(nextTime > 0) { //如果有紀錄上次未生成怪物時間
                     random = nextTime; //覆蓋掉隨機時間
@@ -122,17 +157,46 @@ public class Loading extends Activity {
         startActivity(i);
     }
 
-    public void getRarityData(String scene){
+    public void getData(String scene){
         GameDBHelper helper = GameDBHelper.getInstance(this);
-        String[] column = { "_id", "rareWeight","scene_1", "scene_2"};
-        Cursor c = helper.getReadableDatabase().query("mobsdata", column, "scene_" + scene + "=?", new String[]{"1"}, null, null, null);
+//        String[] column = { "_id", "rareWeight","scene_1", "scene_2"};
+        Cursor c = helper.getReadableDatabase().query("mobsdata", null, "scene_" + scene + "=?", new String[]{"1"}, null, null, null);
 
         c.moveToFirst();
         for (int i = 0; i < c.getCount(); i++) {
             String id = c.getString(c.getColumnIndex("_id"));
+            String name = c.getString(c.getColumnIndex("name"));
+            int hp = c.getInt(c.getColumnIndex("healthPoint"));
             int rarity = c.getInt(c.getColumnIndex("rareWeight"));
+            int speed = c.getInt(c.getColumnIndex("speed"));
+            int qCounts = c.getInt(c.getColumnIndex("qCounts"));
+            int qTypes = c.getInt(c.getColumnIndex("qTypes"));
+            int qRange = c.getInt(c.getColumnIndex("qRange"));
+            int stunTime = c.getInt(c.getColumnIndex("stunTime"));
+            int atk = c.getInt(c.getColumnIndex("atk"));
+            String imageR = c.getString(c.getColumnIndex("image_R"));
+            int loots_1 = c.getInt(c.getColumnIndex("loots_1"));
+            int loots_2 = c.getInt(c.getColumnIndex("loots_2"));
+            int loots_3 = c.getInt(c.getColumnIndex("loots_3"));
+            int loots_1_dp = c.getInt(c.getColumnIndex("loots_1_dp"));
+            int loots_2_dp = c.getInt(c.getColumnIndex("loots_2_dp"));
+            int loots_3_dp = c.getInt(c.getColumnIndex("loots_3_dp"));
+            int lootsInside[] = {loots_1,loots_2,loots_3};
+            int lootsdpInside[] = {loots_1_dp,loots_2_dp,loots_3_dp};
             idList.add(id);
+            nameList.add(name);
+            healthPointList.add(hp);
             rarityList.add(rarity);
+            speedList.add(speed);
+            qCountsList.add(qCounts);
+            qTypesList.add(qTypes);
+            qRangeList.add(qRange);
+            stunTimeList.add(stunTime);
+            atkList.add(atk);
+            imageList.add(imageR);
+            lootsList.add(lootsInside);
+            lootsDropRateList.add(lootsdpInside);
+
             c.moveToNext();
         }
         c.close();
@@ -144,17 +208,46 @@ public class Loading extends Activity {
         randomTest.randomTest(); //執行運算方法
     }
 
-    public void copyDBFile() {
+    public void getPlayerStatus(){
+        try {
+            InputStream is = this.getResources().openRawResource(R.raw.playerdata);
+            byte[] buffer = new byte[is.available()];
+            is.read(buffer);
+            String json = new String(buffer, "UTF-8");
+            Gson gson = new Gson();
+            Player playInfo = gson.fromJson(json, Player.class);
+            Log.d(TAG2,"玩家名稱 : " + String.valueOf(playInfo.playerStatus[0].playerID));
+            Log.d(TAG2,"玩家攻擊力 : " + String.valueOf(playInfo.playerStatus[0].playerATK));
+            Log.d(TAG2,"玩家目前HP : " + String.valueOf(playInfo.playerStatus[0].playerCurrentHP));
+            Log.d(TAG2,"玩家最大HP : " + String.valueOf(playInfo.playerStatus[0].playerMaxHP));
+
+            Log.d(TAG2,"玩家100101號道具存量 : " + String.valueOf(playInfo.playerInventory[0].i100101));
+            Log.d(TAG2,"怪物圖鑑1001號生態介紹解鎖狀態 : " + String.valueOf(playInfo.playerMobsCollection[0].m1001.mobsBio));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void copyDBFile()
+    {
         try {
             File f = new File(DBInfo.DB_FILE);
-            Log.d("GameDBHelper", "" + DBInfo.DB_FILE);
-            if (!f.exists()) {
+            File dbDir = new File(DBInfo.DB_FILE.substring(0,DBInfo.DB_FILE.length()-12));
+            Log.d("GameDBHelper", "copyFiles : "+DBInfo.DB_FILE);
+            dbDir.mkdirs();
+            if (! f.exists())
+            {
+
                 InputStream is = getResources().openRawResource(R.raw.idlegame);
                 OutputStream os = new FileOutputStream(DBInfo.DB_FILE);
                 int read;
-                while ((read = is.read()) != -1) {
+                Log.d("GameDBHelper", "Start Copy");
+                while ((read = is.read()) != -1)
+                {
                     os.write(read);
                 }
+                Log.d("GameDBHelper", "FilesCopied");
                 os.close();
                 is.close();
             }
--- app/src/main/java/com/example/alucardc/idlegame/MainActivity.java
+++ app/src/main/java/com/example/alucardc/idlegame/MainActivity.java
@@ -29,22 +29,6 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    String TAG = "MainDataTest";
-    String TAG2 = "MainPlayerTest";
-    public static ArrayList idList = new ArrayList();
-    public static ArrayList nameList = new ArrayList();
-    public static ArrayList healthPointList = new ArrayList();
-    public static ArrayList rarityList = new ArrayList();
-    public static ArrayList speedList = new ArrayList();
-    public static ArrayList qCountsList = new ArrayList();
-    public static ArrayList qTypesList = new ArrayList();
-    public static ArrayList qRangeList = new ArrayList();
-    public static ArrayList stunTimeList = new ArrayList();
-    public static ArrayList atkList = new ArrayList();
-    public static ArrayList imageList = new ArrayList();
-    public static ArrayList lootsList = new ArrayList();
-    public static ArrayList lootsDropRateList = new ArrayList();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  //隱藏狀態列
@@ -55,27 +39,6 @@ public class MainActivity extends AppCompatActivity {
         DBInfo.DB_FILE = getDatabasePath("idlegame")+".db";    //database的絕對路徑
 //        DBInfo.JSON_FILE = getFilesDir("playerdata.json");
 
-        copyDBFile();
-
-        getData("1");
-        getPlayerStatus();
-
-        Log.d(TAG, idList.toString());
-        Log.d(TAG, nameList.toString());
-        Log.d(TAG, healthPointList.toString());
-        Log.d(TAG, rarityList.toString());
-        Log.d(TAG, speedList.toString());
-        Log.d(TAG, qCountsList.toString());
-        Log.d(TAG, qTypesList.toString());
-        Log.d(TAG, qRangeList.toString());
-        Log.d(TAG, stunTimeList.toString());
-        Log.d(TAG, atkList.toString());
-        Log.d(TAG, imageList.toString());
-        int[] loots = (int[]) lootsList.get(0);
-        Log.d(TAG, loots[0]+", "+loots[1]+", "+loots[2]);
-        int[] lootsDP = (int[]) lootsDropRateList.get(0);
-        Log.d(TAG, lootsDP[0]+", "+lootsDP[1]+", "+lootsDP[2]);
-
     }
     public void enter_scene_1(View view){
         Intent it = new Intent();
@@ -83,136 +46,39 @@ public class MainActivity extends AppCompatActivity {
         startActivity(it);
     }
 
-    public void getPlayerStatus(){
-        try {
-            InputStream is = this.getResources().openRawResource(R.raw.playerdata);
-            byte[] buffer = new byte[is.available()];
-            is.read(buffer);
-            String json = new String(buffer, "UTF-8");
-            Gson gson = new Gson();
-            Player playInfo = gson.fromJson(json, Player.class);
-            Log.d(TAG2,"玩家名稱 : " + String.valueOf(playInfo.playerStatus[0].playerID));
-            Log.d(TAG2,"玩家攻擊力 : " + String.valueOf(playInfo.playerStatus[0].playerATK));
-            Log.d(TAG2,"玩家目前HP : " + String.valueOf(playInfo.playerStatus[0].playerCurrentHP));
-            Log.d(TAG2,"玩家最大HP : " + String.valueOf(playInfo.playerStatus[0].playerMaxHP));
-
-            Log.d(TAG2,"玩家100101號道具存量 : " + String.valueOf(playInfo.playerInventory[0].i100101));
-            Log.d(TAG2,"怪物圖鑑1001號生態介紹解鎖狀態 : " + String.valueOf(playInfo.playerMobsCollection[0].m1001.mobsBio));
-
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void getData(){
-        GameDBHelper helper = GameDBHelper.getInstance(this);
-        String[] column = { "_id", "name", "healthPoint"};
-        Cursor c = helper.getReadableDatabase().query("mobsdata", column, "healthPoint>?", new String[]{"10"}, null, null, null);
-
-        c.moveToFirst();
-        for (int i = 0; i < c.getCount(); i++) {
-            String id = c.getString(c.getColumnIndex("_id"));
-            String name = c.getString(c.getColumnIndex("name"));
-            int hp = c.getInt(c.getColumnIndex("healthPoint"));
-            c.moveToNext();
-            idList.add(id);
-            nameList.add(name);
-            healthPointList.add(hp);
-        }
-        c.close();
-    }
-
-    public void getData(String scene1, String scene2){
-        GameDBHelper helper = GameDBHelper.getInstance(this);
-        String[] column = { "_id", "rareWeight","scene_1", "scene_2"};
-        Cursor c = helper.getReadableDatabase().query("mobsdata", column, "scene_1=? AND scene_2=?", new String[]{scene1,scene2}, null, null, null);
-
-        c.moveToFirst();
-        for (int i = 0; i < c.getCount(); i++) {
-            String id = c.getString(c.getColumnIndex("_id"));
-            int rarity = c.getInt(c.getColumnIndex("rareWeight"));
-            idList.add(id);
-            rarityList.add(rarity);
-            c.moveToNext();
-        }
-        c.close();
-    }
-
-    public void getData(String scene){
-        GameDBHelper helper = GameDBHelper.getInstance(this);
+//    public void getData(){
+//        GameDBHelper helper = GameDBHelper.getInstance(this);
+//        String[] column = { "_id", "name", "healthPoint"};
+//        Cursor c = helper.getReadableDatabase().query("mobsdata", column, "healthPoint>?", new String[]{"10"}, null, null, null);
+//
+//        c.moveToFirst();
+//        for (int i = 0; i < c.getCount(); i++) {
+//            String id = c.getString(c.getColumnIndex("_id"));
+//            String name = c.getString(c.getColumnIndex("name"));
+//            int hp = c.getInt(c.getColumnIndex("healthPoint"));
+//            c.moveToNext();
+//            idList.add(id);
+//            nameList.add(name);
+//            healthPointList.add(hp);
+//        }
+//        c.close();
+//    }
+//
+//    public void getData(String scene1, String scene2){
+//        GameDBHelper helper = GameDBHelper.getInstance(this);
 //        String[] column = { "_id", "rareWeight","scene_1", "scene_2"};
-        Cursor c = helper.getReadableDatabase().query("mobsdata", null, "scene_" + scene + "=?", new String[]{"1"}, null, null, null);
-
-        c.moveToFirst();
-        for (int i = 0; i < c.getCount(); i++) {
-            String id = c.getString(c.getColumnIndex("_id"));
-            String name = c.getString(c.getColumnIndex("name"));
-            int hp = c.getInt(c.getColumnIndex("healthPoint"));
-            int rarity = c.getInt(c.getColumnIndex("rareWeight"));
-            int speed = c.getInt(c.getColumnIndex("speed"));
-            int qCounts = c.getInt(c.getColumnIndex("qCounts"));
-            int qTypes = c.getInt(c.getColumnIndex("qTypes"));
-            int qRange = c.getInt(c.getColumnIndex("qRange"));
-            int stunTime = c.getInt(c.getColumnIndex("stunTime"));
-            int atk = c.getInt(c.getColumnIndex("atk"));
-            String imageR = c.getString(c.getColumnIndex("image_R"));
-            int loots_1 = c.getInt(c.getColumnIndex("loots_1"));
-            int loots_2 = c.getInt(c.getColumnIndex("loots_2"));
-            int loots_3 = c.getInt(c.getColumnIndex("loots_3"));
-            int loots_1_dp = c.getInt(c.getColumnIndex("loots_1_dp"));
-            int loots_2_dp = c.getInt(c.getColumnIndex("loots_2_dp"));
-            int loots_3_dp = c.getInt(c.getColumnIndex("loots_3_dp"));
-            int lootsInside[] = {loots_1,loots_2,loots_3};
-            int lootsdpInside[] = {loots_1_dp,loots_2_dp,loots_3_dp};
-            idList.add(id);
-            nameList.add(name);
-            healthPointList.add(hp);
-            rarityList.add(rarity);
-            speedList.add(speed);
-            qCountsList.add(qCounts);
-            qTypesList.add(qTypes);
-            qRangeList.add(qRange);
-            stunTimeList.add(stunTime);
-            atkList.add(atk);
-            imageList.add(imageR);
-            lootsList.add(lootsInside);
-            lootsDropRateList.add(lootsdpInside);
-
-            c.moveToNext();
-        }
-        c.close();
-    }
-
-    public void copyDBFile()
-    {
-        try {
-            File f = new File(DBInfo.DB_FILE);
-            File dbDir = new File(DBInfo.DB_FILE.substring(0,DBInfo.DB_FILE.length()-12));
-            Log.d("GameDBHelper", "copyFiles : "+DBInfo.DB_FILE);
-            dbDir.mkdirs();
-            if (! f.exists())
-            {
-
-                InputStream is = getResources().openRawResource(R.raw.idlegame);
-                OutputStream os = new FileOutputStream(DBInfo.DB_FILE);
-                int read;
-                Log.d("GameDBHelper", "Start Copy");
-                while ((read = is.read()) != -1)
-                {
-                    os.write(read);
-                }
-                Log.d("GameDBHelper", "FilesCopied");
-                os.close();
-                is.close();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//        Cursor c = helper.getReadableDatabase().query("mobsdata", column, "scene_1=? AND scene_2=?", new String[]{scene1,scene2}, null, null, null);
+//
+//        c.moveToFirst();
+//        for (int i = 0; i < c.getCount(); i++) {
+//            String id = c.getString(c.getColumnIndex("_id"));
+//            int rarity = c.getInt(c.getColumnIndex("rareWeight"));
+//            idList.add(id);
+//            rarityList.add(rarity);
+//            c.moveToNext();
+//        }
+//        c.close();
+//    }
 
     public void copyJSONFile()
     {
