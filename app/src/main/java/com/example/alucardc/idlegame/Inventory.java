be3eb1825af31180743f2fe45cba499a985a13da
Cratters <4a2k0055@stust.edu.tw>
2017-12-19 23:56:26 +0800

    v19 Inventory

--- app/src/main/java/com/example/alucardc/idlegame/Inventory.java
+++ app/src/main/java/com/example/alucardc/idlegame/Inventory.java
@@ -31,7 +31,7 @@ import java.util.List;
 
 public class Inventory extends DialogFragment {
 
-    final int column = 3;
+    final int column = 4;
     View v,item;
     Context context;
     LinearLayout inventory_bottomBag;
